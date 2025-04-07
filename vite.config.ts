6fb23fcfe7d3bf66e173298dc22b710afbcece95
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2025-04-07 17:15:19 -0300

    PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Shadow Division",
+        description: "App para dividir gastos entre personas",
+        theme_color: "#1976d2",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "icons/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
